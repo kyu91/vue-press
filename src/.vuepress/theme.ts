47dccacc2f2813f6fabf032e081bcd927dac48e2
kyu91 <hongsk5689@gmail.com>
2024-08-06 22:57:09 +0900

    포폴 세팅

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -192,9 +192,9 @@ export default hopeTheme({
       // },
 
       // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
+      revealJs: {
+        plugins: ["highlight", "math", "search", "notes", "zoom"],
+      },
 
       // install @vue/repl before enabling it
       // vuePlayground: true,
