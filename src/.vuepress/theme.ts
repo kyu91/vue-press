40feaddf38300e4368cbbb7729f1affcb0fe49a2
kyu91 <hongsk5689@gmail.com>
2024-08-07 05:00:08 +0900

    777777777777777

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -97,134 +97,17 @@ export default hopeTheme({
   plugins: {
     blog: true,
     docsearch: {
-      // appId: process.env.DOCSEARCH_APP_ID,  
-      // apiKey: process.env.DOCSEARCH_APP_KEY,
-      // indexName: process.env.DOCSEARCH_INDEX_NAME,
-      // locales: {
-      //   '/': {
-      //     placeholder: 'Search Documentation',
-      //     translations: {
-      //       button: {
-      //         buttonText: '검색하십쇼',
-      //       },
-      //     },
-      //   },
-      // },
-      appId: process.env.DOCSEARCH_APP_ID,
+      appId: process.env.DOCSEARCH_APP_ID,  
       apiKey: process.env.DOCSEARCH_APP_KEY,
-      rateLimit: 8,
-      startUrls: [
-        // These are urls which algolia start to craw
-        // If your site is divided in to mutiple parts,
-        // you may want to set mutiple entry links
-        'https://www.levih.me/',
-      ],
-      sitemaps: [
-        // if you are using sitemap plugins (e.g.: @vuepress-plugin/sitemap), you may provide one
-        'https://www.levih.me/sitemap.xml',
-      ],
-      ignoreCanonicalTo: false,
-      exclusionPatterns: [
-        // You can use this to stop algolia crawing some paths
-      ],
-      // Crawler schedule, set it according to your docs update frequency
-      schedule: 'at 02:00 every 1 day',
-      actions: [
-        // you may have mutiple actions, especially when you are deploying mutiple docs under one domain
-        {
-          // name the index with name you like
-          indexName: process.env.DOCSEARCH_INDEX_NAME,
-          // paths where the index take effect
-          // pathsToMatch: ['https://YOUR_WEBSITE_URL/**'],
-          // controls how algolia extracts records from your site
-          recordExtractor: ({ $, helpers }) => {
-            // options for @vuepress/theme-default
-            return helpers.docsearch({
-              recordProps: {
-                lvl0: {
-                  selectors: '.vp-sidebar-heading.active',
-                  defaultValue: 'Documentation',
-                },
-                lvl1: '.theme-default-content h1',
-                lvl2: '.theme-default-content h2',
-                lvl3: '.theme-default-content h3',
-                lvl4: '.theme-default-content h4',
-                lvl5: '.theme-default-content h5',
-                lvl6: '.theme-default-content h6',
-                content: '.theme-default-content p, .theme-default-content li',
-              },
-              indexHeadings: true,
-            })
+      indexName: process.env.DOCSEARCH_INDEX_NAME,
+      locales: {
+        '/': {
+          placeholder: 'Search Documentation',
+          translations: {
+            button: {
+              buttonText: '검색하십쇼',
+            },
           },
-        },
-      ],
-      initialIndexSettings: {
-        // controls how index are initialized
-        // only has effects before index are initialize
-        // you may need to delete your index and recraw after modification
-        YOUR_INDEX_NAME: {
-          attributesForFaceting: ['type', 'lang'],
-          attributesToRetrieve: ['hierarchy', 'content', 'anchor', 'url'],
-          attributesToHighlight: ['hierarchy', 'hierarchy_camel', 'content'],
-          attributesToSnippet: ['content:10'],
-          camelCaseAttributes: ['hierarchy', 'hierarchy_radio', 'content'],
-          searchableAttributes: [
-            'unordered(hierarchy_radio_camel.lvl0)',
-            'unordered(hierarchy_radio.lvl0)',
-            'unordered(hierarchy_radio_camel.lvl1)',
-            'unordered(hierarchy_radio.lvl1)',
-            'unordered(hierarchy_radio_camel.lvl2)',
-            'unordered(hierarchy_radio.lvl2)',
-            'unordered(hierarchy_radio_camel.lvl3)',
-            'unordered(hierarchy_radio.lvl3)',
-            'unordered(hierarchy_radio_camel.lvl4)',
-            'unordered(hierarchy_radio.lvl4)',
-            'unordered(hierarchy_radio_camel.lvl5)',
-            'unordered(hierarchy_radio.lvl5)',
-            'unordered(hierarchy_radio_camel.lvl6)',
-            'unordered(hierarchy_radio.lvl6)',
-            'unordered(hierarchy_camel.lvl0)',
-            'unordered(hierarchy.lvl0)',
-            'unordered(hierarchy_camel.lvl1)',
-            'unordered(hierarchy.lvl1)',
-            'unordered(hierarchy_camel.lvl2)',
-            'unordered(hierarchy.lvl2)',
-            'unordered(hierarchy_camel.lvl3)',
-            'unordered(hierarchy.lvl3)',
-            'unordered(hierarchy_camel.lvl4)',
-            'unordered(hierarchy.lvl4)',
-            'unordered(hierarchy_camel.lvl5)',
-            'unordered(hierarchy.lvl5)',
-            'unordered(hierarchy_camel.lvl6)',
-            'unordered(hierarchy.lvl6)',
-            'content',
-          ],
-          distinct: true,
-          attributeForDistinct: 'url',
-          customRanking: [
-            'desc(weight.pageRank)',
-            'desc(weight.level)',
-            'asc(weight.position)',
-          ],
-          ranking: [
-            'words',
-            'filters',
-            'typo',
-            'attribute',
-            'proximity',
-            'exact',
-            'custom',
-          ],
-          highlightPreTag: '<span class="algolia-docsearch-suggestion--highlight">',
-          highlightPostTag: '</span>',
-          minWordSizefor1Typo: 3,
-          minWordSizefor2Typos: 7,
-          allowTyposOnNumericTokens: false,
-          minProximity: 1,
-          ignorePlurals: true,
-          advancedSyntax: true,
-          attributeCriteriaComputedByMinProximity: true,
-          removeWordsIfNoResults: 'allOptional',
         },
       },
     },
