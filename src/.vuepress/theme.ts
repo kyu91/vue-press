2bbefe9b5270c4fe2d03b8fd90c3cf46eaf15ae6
kyu91 <hongsk5689@gmail.com>
2024-08-07 03:00:52 +0900

    wkawha wkwk

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -114,7 +114,7 @@ export default hopeTheme({
       },
     },
 
-    // Install @waline/client before enabling it
+    // Install @waline/client before enabling it 
     // Note: This is for testing ONLY!
     // You MUST generate and use your own comment service in production.
     // comment: {
