d62d3264073242b55926f6f571c89937dbabda61
kyu91 <hongsk5689@gmail.com>
2024-08-08 00:57:10 +0900

    검색 기능 수정

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -100,12 +100,47 @@ export default hopeTheme({
       appId: process.env.DOCSEARCH_APP_ID,  
       apiKey: process.env.DOCSEARCH_APP_KEY,
       indexName: process.env.DOCSEARCH_NAME,
+      searchParameters: {
+        facetFilters: ['tags:v2'],
+      },
       locales: {
         '/': {
           placeholder: 'Search Documentation',
           translations: {
             button: {
               buttonText: '검색하십쇼',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '쿼리 조건 삭제',
+                resetButtonAriaLabel: '쿼리 조건 삭제',
+                cancelButtonText: '취소',
+                cancelButtonAriaLabel: '취소',
+              },
+              startScreen: {
+                recentSearchesTitle: '검색기록',
+                noRecentSearchesText: '검색기록 없습니다.',
+                saveRecentSearchButtonTitle: '검색기록 저장',
+                removeRecentSearchButtonTitle: '검색 기록에서 삭제',
+                favoriteSearchesTitle: '수집',
+                removeFavoriteSearchButtonTitle: '즐겨 찾기에서 삭제',
+              },
+              errorScreen: {
+                titleText: '결과를 얻을 수 없습니다',
+                helpText: '네트워크 연결을 확인해야 할 수도 있습니다.',
+              },
+              footer: {
+                selectText: '선택',
+                navigateText: '전환',
+                closeText: '폐쇄',
+                searchByText: '검색자',
+              },
+              noResultsScreen: {
+                noResultsText: '관련 검색결과가 없습니다.',
+                suggestedQueryText: '당신은 쿼리를 시도할 수 있습니다',
+                reportMissingResultsText: '이 쿼리에 결과가 있어야 한다고 생각하시나요?',
+                reportMissingResultsLinkText: '피드백을 원하시면 클릭하세요',
+              },
             },
           },
         },
