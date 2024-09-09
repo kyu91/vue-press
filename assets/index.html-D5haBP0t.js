7843f8b6a2d2f32d4c0f47da32872ae237b7528d
kyu91 <105551449+kyu91@users.noreply.github.com>
2024-09-09 16:29:26 +0000

    Deploying to gh-pages from @ kyu91/kyu91.github.io@d68e53e21f6a7957104848c21eaaa0d84b52ab27 🚀

---
+++ assets/index.html-D5haBP0t.js
@@ -0,0 +1,88 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as n,o as i}from"./app-C1g0URBT.js";const a={};function o(l,r){return i(),t("div",null,[n(`
+features:
+  - title: Markdown Enhance
+    icon: fab fa-markdown
+    details: Add align, sup/sub script, footnote, tasklist, tex, flowchart, diagram, mark and presentation support in Markdown
+    link: /guide/markdown/
+
+  - title: Slide Page
+    icon: person-chalkboard
+    details: Adding slide pages to display things you like
+    link: /guide/layout/slides
+
+  - title: Layout Enhancement
+    icon: object-group
+    details: Adding breadcrumb, footer, improved navbar, improved page nav and etc.
+    link: /guide/layout/
+
+  - title: Pageviews and Comments
+    icon: comment-dots
+    details: Start pageview statistics and comment support with Waline
+    link: /guide/feature/comment.html
+
+  - title: Article Information
+    icon: circle-info
+    details: Add author, writing date, reading time, word count and other information to your article
+    link: /guide/feature/page-info.html
+
+  - title: Blog Support
+    icon: blog
+    details: Listing your articles with their dates, tags and categories with some awesome layouts
+    link: /guide/blog/
+
+  - title: Customizable Theme Color
+    icon: palette
+    details: Customize theme color
+    link: /guide/interface/theme-color.html
+
+  - title: Dark Mode
+    icon: circle-half-stroke
+    details: Switch between light and dark modes freely
+    link: /guide/interface/darkmode.html
+
+  - title: Full A11y support
+    icon: universal-access
+    details: Full support of accessibility in your site
+    link: /guide/interface/accessibility.html
+
+  - title: Article Encryption
+    icon: lock
+    details: Encrypt you articles based on page links, so that only the one you want could see them
+    link: /guide/feature/encrypt.html
+
+  - title: Search
+    icon: search
+    details: Support docsearch and client search
+    link: /guide/feature/search.html
+
+  - title: Image Preview
+    icon: image
+    details: Support viewing, zooming, sharing your page images like a gallery
+    link: /guide/feature/photo-swipe.html
+
+  - title: SEO Enhancement
+    icon: dumbbell
+    details: Optimize pages for search engines
+    link: /guide/advanced/seo.html
+
+  - title: Sitemap
+    icon: sitemap
+    details: Generate a Sitemap for your site
+    link: /guide/advanced/sitemap.html
+
+  - title: Feed
+    icon: rss
+    details: Generate feed to allow users to subscribe it
+    link: /guide/advanced/feed.html
+
+  - title: PWA
+    icon: mobile-screen
+    details: Make your site more like an APP
+    link: /guide/advanced/pwa.html
+
+  - title: More New Features
+    icon: ellipsis
+    details: Including icon support, fullscreen button, etc.
+    link: /guide/feature/ %%
+
+`)])}const c=e(a,[["render",o],["__file","index.html.vue"]]),m=JSON.parse(`{"path":"/","title":"Home","lang":"en-US","frontmatter":{"home":true,"icon":"home","title":"Home","heroText":"Welecome!","tagline":"This is a blog I created for fun, Enjoy it 😏","actions":[{"text":"Go to Article","link":"/article/","icon":"signs-post","type":"primary"},{"text":"Contact me","icon":"lightbulb","link":"/#tally-open=3Xq50O&tally-layout=modal&tally-emoji-text=%F0%9F%91%8B&tally-emoji-animation=wave/"}],"copyright":false,"footer":"MIT Licensed | Copyright © 2024-Levi","head":[["meta",{"property":"og:url","content":"https://www.levih.me/"}],["meta",{"property":"og:site_name","content":"It's Blog"}],["meta",{"property":"og:title","content":"Home"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2024-09-06T02:26:58.000Z"}],["meta",{"property":"article:author","content":"Levi"}],["meta",{"property":"article:modified_time","content":"2024-09-06T02:26:58.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"Home\\"}"]]},"headers":[],"git":{"createdTime":1722844238000,"updatedTime":1725589618000,"contributors":[{"name":"대웅펫","email":"hongsk5689@gmail.com","commits":9}]},"readingTime":{"minutes":1.21,"words":362},"filePathRelative":"README.md","localizedDate":"August 5, 2024","excerpt":"<!--\\nfeatures:\\n  - title: Markdown Enhance\\n    icon: fab fa-markdown\\n    details: Add align, sup/sub script, footnote, tasklist, tex, flowchart, diagram, mark and presentation support in Markdown\\n    link: /guide/markdown/\\n\\n  - title: Slide Page\\n    icon: person-chalkboard\\n    details: Adding slide pages to display things you like\\n    link: /guide/layout/slides\\n\\n  - title: Layout Enhancement\\n    icon: object-group\\n    details: Adding breadcrumb, footer, improved navbar, improved page nav and etc.\\n    link: /guide/layout/\\n\\n  - title: Pageviews and Comments\\n    icon: comment-dots\\n    details: Start pageview statistics and comment support with Waline\\n    link: /guide/feature/comment.html\\n\\n  - title: Article Information\\n    icon: circle-info\\n    details: Add author, writing date, reading time, word count and other information to your article\\n    link: /guide/feature/page-info.html\\n\\n  - title: Blog Support\\n    icon: blog\\n    details: Listing your articles with their dates, tags and categories with some awesome layouts\\n    link: /guide/blog/\\n\\n  - title: Customizable Theme Color\\n    icon: palette\\n    details: Customize theme color\\n    link: /guide/interface/theme-color.html\\n\\n  - title: Dark Mode\\n    icon: circle-half-stroke\\n    details: Switch between light and dark modes freely\\n    link: /guide/interface/darkmode.html\\n\\n  - title: Full A11y support\\n    icon: universal-access\\n    details: Full support of accessibility in your site\\n    link: /guide/interface/accessibility.html\\n\\n  - title: Article Encryption\\n    icon: lock\\n    details: Encrypt you articles based on page links, so that only the one you want could see them\\n    link: /guide/feature/encrypt.html\\n\\n  - title: Search\\n    icon: search\\n    details: Support docsearch and client search\\n    link: /guide/feature/search.html\\n\\n  - title: Image Preview\\n    icon: image\\n    details: Support viewing, zooming, sharing your page images like a gallery\\n    link: /guide/feature/photo-swipe.html\\n\\n  - title: SEO Enhancement\\n    icon: dumbbell\\n    details: Optimize pages for search engines\\n    link: /guide/advanced/seo.html\\n\\n  - title: Sitemap\\n    icon: sitemap\\n    details: Generate a Sitemap for your site\\n    link: /guide/advanced/sitemap.html\\n\\n  - title: Feed\\n    icon: rss\\n    details: Generate feed to allow users to subscribe it\\n    link: /guide/advanced/feed.html\\n\\n  - title: PWA\\n    icon: mobile-screen\\n    details: Make your site more like an APP\\n    link: /guide/advanced/pwa.html\\n\\n  - title: More New Features\\n    icon: ellipsis\\n    details: Including icon support, fullscreen button, etc.\\n    link: /guide/feature/ %%\\n\\n-->"}`);export{c as comp,m as data};
