e483ee4280f17f660e15d17b884e02a0f5b8360b
seok-kyu <105551449+kyu91@users.noreply.github.com>
2025-02-28 23:56:29 +0900

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/vue-press/",
 
   lang: "ko",
   title: "It's Blog",
